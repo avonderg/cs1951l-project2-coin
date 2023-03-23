6cfc5d09a629308634bed2a2bc2be1efe4cc234e
avonderg <77714730+avonderg@users.noreply.github.com>
2023-03-23 13:57:34 -0400

    hmm

--- test/testing_utils.go
+++ test/testing_utils.go
@@ -199,6 +199,28 @@ func AssertSize(t *testing.T, actualSize int, expectedSize int) {
 	}
 }
 
+func TestMine1(t *testing.T) {
+	utils.SetDebug(true)
+
+	nodes := NewCluster(5)
+	var chains []*blockchain.BlockChain
+	for _, node := range nodes {
+		chains = append(chains, node.BlockChain)
+	}
+	defer CleanUp(chains)
+	//genBlock := blockchain.GenesisBlock(blockchain.DefaultConfig())
+	//chain := blockchain.New(blockchain.DefaultConfig())
+	StartCluster(nodes)
+	user := nodes[1]
+	t.Errorf("hello\n")
+	FillWalletWithCoins(user.Wallet, 10, 250)
+	if user.Wallet.Balance != 250*10 {
+		t.Errorf("user has wrong number of coins in its wallet")
+	}
+	//StartMiners(nodes)
+	//defer CleanUp()
+}
+
 func CheckTransactionInTXPool(t *testing.T, n *pkg.Node, tx *block.Transaction) {
 	t.Helper()
 	if !n.Miner.TxPool.TxQ.Has(tx) {
