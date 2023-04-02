b6b384dfdab7c455eb20163a6eb572e41f122c35
avonderg <77714730+avonderg@users.noreply.github.com>
2023-04-02 16:28:24 -0400

    asdf

--- pkg/wallet/wallet.go
+++ pkg/wallet/wallet.go
@@ -174,26 +174,16 @@ func (w *Wallet) checkInputs(tx *block.Transaction) {
 	//		}
 	//	}
 	//}
-	//hash := tx.Hash()
-	//if _, ok := w.UnseenSpentCoins[hash]; ok { // if spent
-	//	coinInfo := w.UnseenSpentCoins[hash]
-	//
-	//	delete(w.UnseenSpentCoins, hash)
-	//
-	//	for _, coin := range coinInfo {
-	//		w.UnconfirmedSpentCoins[coin] = 0
-	//	}
-	//}
-	inps := tx.Inputs
-	for _, input := range inps {
-		if _, ok := w.UnseenSpentCoins[input.ReferenceTransactionHash]; ok { // if spent
-			coinInfo := w.UnseenSpentCoins[input.ReferenceTransactionHash]
+	hash := tx.Hash()
+	if _, ok := w.UnseenSpentCoins[hash]; ok { // if spent
+		coinInfo := w.UnseenSpentCoins[hash]
 
-			delete(w.UnseenSpentCoins, input.ReferenceTransactionHash)
+		delete(w.UnseenSpentCoins, hash)
 
-			for _, coin := range coinInfo {
-				w.UnconfirmedSpentCoins[coin] = 0
-			}
+		for i, coin := range coinInfo {
+			w.UnconfirmedSpentCoins[coin] = 0
+			w.UnseenSpentCoins[hash] = append(coinInfo[:i], coinInfo[i+1:]...)
+
 		}
 	}
 }
